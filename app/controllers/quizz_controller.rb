0189e4b53c547d2f3be9edcd4fa51174f86ea508
HninHninAye <hninhninaye@cloudsoure.co.jp>
2016-08-07 16:03:58 +0630

    change route

--- app/controllers/quizz_controller.rb
+++ app/controllers/quizz_controller.rb
@@ -1,6 +1,6 @@
 class QuizzController < ApplicationController
-  def index
-    quizz = Category.where(name: params[:category]).first.quizzes
+  def show
+    quizz = Category.where(name: params[:id]).first.quizzes
     render :json => {:quiz => Quiz.get_quizz(quizz)}
   end
 end
