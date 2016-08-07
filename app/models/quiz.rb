12cb83bc2e1fafe7ad71a76eb14864a2efbea862
HninHninAye <hninhninaye@cloudsoure.co.jp>
2016-08-07 15:43:23 +0630

    quiz_json

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,3 +1,15 @@
 class Quiz < ApplicationRecord
   belongs_to :category
+  has_many :answers
+
+  def self.get_quizz quizzes
+    quizzes.collect do |quiz|
+      {
+        id: quiz.id,
+        question: quiz.question,
+        answers: quiz.answers.map(&:ans_name),
+        collect: quiz.collect
+      }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "category#index"
   get 'category/index'
-  get 'quizz/index'
+  get 'quizz' => 'quizz#index'
 
   resources :ranking
 
