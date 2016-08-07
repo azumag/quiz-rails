9f75a8c053438e0e79fefb885badc5a50be8066d
HninHninAye <hninhninaye@cloudsoure.co.jp>
2016-08-07 15:43:23 +0630

    quiz_json

--- app/controllers/quizz_controller.rb
+++ app/controllers/quizz_controller.rb
@@ -1,4 +1,6 @@
 class QuizzController < ApplicationController
   def index
+    quizz = Category.where(name: params[:category]).first.quizzes
+    render :json => {:quiz => Quiz.get_quizz(quizz)}
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   has_many :scores
+  has_many :quizzes
 end
