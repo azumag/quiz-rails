e1dff0a73dd9789f64a68b2479f54cea3eb3baf9
azumag <tsubasa.azumagakito@gmail.com>
2016-08-07 10:31:46 +0630

    setings

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,8 +1,5 @@
 class CategoryController < ApplicationController
   def index
-    Category.create(name: 'Yangon')
-    Category.create(name: 'IT')
-    Category.create(name: 'Japan')
     render :json => {:category => Category.all.as_json(:only => [:name]) }
   end
 end
