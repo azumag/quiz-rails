085516e6d0c6879e242ba1d7bb9b1c91a76669fb
azumag <tsubasa.azumagakito@gmail.com>
2016-08-07 10:21:37 +0630

    ns

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,8 +1,8 @@
 class CategoryController < ApplicationController
   def index
-    Category.find_or_create(name: 'Yangon')
-    Category.find_or_create(name: 'IT')
-    Category.find_or_create(name: 'Japan') 
+    Category.create(name: 'Yangon')
+    Category.create(name: 'IT')
+    Category.create(name: 'Japan')
     render :json => {:category => Category.all.as_json(:only => [:name]) }
   end
 end
