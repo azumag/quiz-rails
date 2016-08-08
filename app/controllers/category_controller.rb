2dc0521d8abb2d927d2f15f48cefd78387b7b956
azumag <tsubasa.azumagakito@gmail.com>
2016-08-08 17:06:05 +0630

    save

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -2,4 +2,9 @@ class CategoryController < ApplicationController
   def index
     render :json => {:category => Category.all.as_json(:only => [:name]) }
   end
+
+  def new
+    @category = Category.new()
+  end
+
 end
